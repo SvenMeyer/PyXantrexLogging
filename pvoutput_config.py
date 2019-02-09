a7429787d9a5281ced198dbc5905f6c7db75de8e
Sven Meyer <git_dev@svenmeyer.com>
2019-02-09 20:41:08 +1100

    no API key

--- pvoutput_config.py
+++ pvoutput_config.py
@@ -4,9 +4,7 @@
 PVOutputURL = "http://pvoutput.org"
 StatusURL = PVOutputURL + "/service/r2/addstatus.jsp"
 OutputURL = PVOutputURL + "/service/r2/addoutput.jsp"
-# user : cumbalum2478
-# System Name : XantrexCumbalum
-APIKey = "bb881d2f59637d17ef1267ea535c2bdee47e74aa"
-SystemID = "64784"
+APIKey   = "###_insert-your-API-key-here_###"
+SystemID = "#####"
 
 #API and System ID can be found on the PVOutput account page - http://pvoutput.org/account.jsp 
